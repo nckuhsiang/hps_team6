69644981de472747ffd3d47b9cd9a75d26ee58b0
nckuhsiang <chench@locust.csie.ncku.edu.tw>
2022-08-09 14:05:12 +0800

    新增註解解說程式碼

--- API/UserAPI.py
+++ API/UserAPI.py
@@ -1,7 +1,18 @@
-from sre_constants import SUCCESS
 import pymysql
+import hashlib
 
+"""
+input:
+    account(string)
+    password(string)
+output:
+    result(bool):create success or fail
+"""
 def createUser(account,password):
+    result = True
+    md5_hash = hashlib.md5()
+    md5_hash.update(password.encode())
+    password = md5_hash.hexdigest()
     db = pymysql.connect(
     host= '34.80.39.159',
     user='chench',
@@ -11,30 +22,21 @@ def createUser(account,password):
     try:
         cursor.execute(sql)
         db.commit()
-        print("success")
     except:
         db.rollback()
-        print('fail')
+        result = False
+        print('create fail,please check the data')
     db.close()
-
-def listUser(account,password):
-    db = pymysql.connect(
-    host= '34.80.39.159',
-    user='chench',
-    database= 'Fiteat')
-    cursor = db.cursor()
-    sql = 'SELECT * FROM User'
-    try:
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for row in results:
-            print(row[0])
-    except:
-        db.rollback()
-        print('fail')
-    db.close()
-
+    return result
+         
+"""
+input:
+    account(string),height(float),weight(float),workload(string),BMI(float),TDEE(float)
+output:
+    result(bool):update success or fail
+"""
 def updateUser(account,height,weight,workload,BMI,TDEE):
+    result = True
     db = pymysql.connect(
     host= '34.80.39.159',
     user='chench',
@@ -44,13 +46,22 @@ def updateUser(account,height,weight,workload,BMI,TDEE):
     try:
         cursor.execute(sql)
         db.commit()
-        print("success")
     except:
         db.rollback()
-        print('fail')
+        result = False
+        print('update fail,please check the data')
     db.close()
+    return result
+    
 
+"""
+input:
+    account(string):account that you want to delete
+output:
+    result(bool):delete success or fail
+"""
 def deleteUser(account):
+    result = True
     db = pymysql.connect(
     host= '34.80.39.159',
     user='chench',
@@ -60,14 +71,37 @@ def deleteUser(account):
     try:
         cursor.execute(sql)
         db.commit()
-        print("success")
     except:
         db.rollback()
-        print('fail')
+        result = False
+        print('delete fail, please check the data')
     db.close()
+    return result
 
+
+"""
+input:
+    account(string)
+output:
+    info(tuple): (height,weight,workload,BMI,TDEE),if accunt is not exist return None
+"""
+def searchUser(account):
+    db = pymysql.connect(
+    host= '34.80.39.159',
+    user='chench',
+    database= 'Fiteat')
+    cursor = db.cursor()
+    sql = "SELECT * FROM User WHERE account='" + account + "';"
+    info = None
+    try:
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        info = (row[2],row[3],row[4],row[5],row[6])
+    except:
+        db.rollback()
+        print('this account is not exist')
+    db.close()
+    return  info
 
 if __name__ == "__main__":
-    # updateUser("test00",177,67,'light',19.5,1733)
-    deleteUser("test00")
-
+    print('start')
