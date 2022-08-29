5a3388ae3203341a0ab5cf99f3ba773a0e97e4fe
Yenzu <yenzu.ee08@nycu.edu.tw>
2022-08-29 11:54:50 +0800

    modify main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # Add module to syspath
 import sys, os
 curr_path = os.path.dirname(os.path.abspath(__file__))
+
+'''
+# for Windows
 sys.path.append(curr_path+'\\GUI')  # import GUI_path
 sys.path.append(curr_path+'\\API')  # import API_path
+'''
+# for Linux
+sys.path.append(curr_path+'/GUI')  # import GUI_path
+sys.path.append(curr_path+'/API')  # import API_path
+
 
 from MainWindow import *
 
