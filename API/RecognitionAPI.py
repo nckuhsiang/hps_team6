199f44e17698741139f9edd2617622b1f466f210
nckuhsiang <chench@locust.csie.ncku.edu.tw>
2022-08-31 16:39:39 +0800

    add recognitionAPI

--- API/RecognitionAPI.py
+++ API/RecognitionAPI.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 # import tensorflow as tf
 import tflite_runtime.interpreter as tflite
-import json
 
 def get_food_by_recognition(image):
     model_name = "mobilenetv2"
@@ -24,29 +23,6 @@ def get_food_by_recognition(image):
     print(prediction,classes_name,npmax)
     return classes_name
 
-
-
-model_name = "mobilenetv2"
-# tf_lite_path = "{}/{}.tflite".format(dir,model_name)
-tf_lite_path = "{}.tflite".format(model_name)
-
-# # Load the TFLite model and allocate tensors.
-# interpreter = tf.lite.Interpreter(tf_lite_path)
-# interpreter.allocate_tensors()
-
-# Load the TFLite model and allocate tensors.
-interpreter = tflite.Interpreter(tf_lite_path)
-interpreter.allocate_tensors()
-
-# load the classes in json file
-classes = ['Apple', 'Banana', 'Grape', 'Guava', 'Tomato', 'dumplings', \
-    'french_fries', 'ice_cream', 'other', 'pizza', 'steak', 'sushi']
-
-# setting the camera
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
 def image_process_to_tensor(img):
     image = cv2.resize(img,(224,224))        
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -54,7 +30,7 @@ def image_process_to_tensor(img):
     tensor = (np.expand_dims(image, axis=0).astype(np.float32))/255.0
     return tensor
 
-def predict(input_tensor,interpreter,classes_name,threshold=0.55):
+def predict(input_tensor,interpreter,classes,threshold=0.55):
     '''
     @input_tensor: input tensor(processed image)
     @interpreter: tf-lite interpreter
@@ -65,7 +41,7 @@ def predict(input_tensor,interpreter,classes_name,threshold=0.55):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
     
-    interpreter.set_tensor(input_details[0]['index'], tensor)
+    interpreter.set_tensor(input_details[0]['index'], input_tensor)
     interpreter.invoke()
     
     # Show predict results
@@ -76,26 +52,8 @@ def predict(input_tensor,interpreter,classes_name,threshold=0.55):
     if max_prediction > threshold and predicted_index != 8 and predicted_index != 11:
         return max_prediction,classes[predicted_index],np.max(prediction_scores)
     else :
-        return -1,classes_name[predicted_index],np.max(prediction_scores)
+        return -1,classes[predicted_index],np.max(prediction_scores)
 
-
-while True:
-    _, frame = cap.read()
-    
-    # Show the frame      
-    cv2.imshow("Prediction", frame)
-    
-    # Preporcess the image to tensor
-    tensor = image_process_to_tensor(frame)
-    index , name , accuracy= predict(tensor,interpreter,classes,threshold=0.6)
-    if index != -1:
-        print(index , name , accuracy)
-    
-    key=cv2.waitKey(1)
-    if key == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
-
-
-# random sample from [0 , 1 , 1]
+# if __name__ == "__main__":
+#     img = cv2.imread("./pizza.jpeg")
+#     print(get_food_by_recognition(img))
